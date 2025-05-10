2d347604c2b93a8eb96f623b362048ee465132ad
misostack <misostack.com@gmail.com>
2025-05-10 18:46:43 +0700

    Add functions playground

--- Functions.playground/Contents.swift
+++ Functions.playground/Contents.swift
@@ -1,3 +1,17 @@
-import UIKit
+import Foundation
 
-var greeting = "Hello, playground"
+func helloSwift() {
+    let today: Date = Date()
+    print("Hello friends, today is \(today.ISO8601Format())!")
+}
+
+func swapValue(a: Int, b: Int) -> (Int, Int) {
+    return (b, a)
+}
+
+helloSwift()
+var a = 2
+var b = 3
+print(a, b)
+(a,b) = swapValue(a:a, b:b)
+print(a, b)
